d4c5d3f25a5cad1710e37d05ce65a2058ef3f3d9
dalaba7046 <49179942+dalaba7046@users.noreply.github.com>
2019-10-31 13:04:48 +0800

    Update 591xin_bei.py

--- 591xin_bei.py
+++ 591xin_bei.py
@@ -37,7 +37,7 @@ try:
             'Connection': 'keep-alive',
             'Cookie': 'PHPSESSID=k0gingasp4ndf66qnkqpaokon1; urlJumpIp=1; urlJumpIpByTxt=%E5%8F%B0%E5%8C%97%E5%B8%82; new_rent_list_kind_test=0; 591_new_session=eyJpdiI6IjF6cDlldGI4c0t0Q29tSVJ4UHJ6UEE9PSIsInZhbHVlIjoia2IwUzBRQnRwZFZLSW1TeVJMY1hDUXdTdHRxZjFHU0t6VXFOZHVDaXhPQ3JnaE8rVFJMcHVvK3JjZjFtVVVpbzVTeVE3SHViVmFQd0ZpVXpzR3VpMFE9PSIsIm1hYyI6IjQ5YWJhMzU3MDY2YWEyMWQ3M2NkMzM1ZGUxNDJlNWFiYzZiMGMwMzIwOWMyYzcyOWI5MWU4NWM5NjA0OWZiYTkifQ%3D%3D; T591_TOKEN=k0gingasp4ndf66qnkqpaokon1; c10f3143a018a0513ebe1e8d27b5391c=1; _ga=GA1.3.1587201468.1572241758; _gid=GA1.3.1335629771.1572241758; _ga=GA1.4.1587201468.1572241758; _gid=GA1.4.1335629771.1572241758; XSRF-TOKEN=eyJpdiI6ImZ2NmY1dEhQT0FRMk9BT0l1WksycEE9PSIsInZhbHVlIjoiVnFLM25WbmU3MWw2bkd0VzdCT2l4cGhhSFNBdk9LNjhTbDNyMXRaUnZKVkIrUE96ZFRVcmVUMUx5bWRSY2JTdjZ0U2xXeGNjNEFac1BtTnVIYlRsaGc9PSIsIm1hYyI6IjBkYTM2MTFhMzU5NDkyYWU5ODYzZWQ0ZmRlZTc0YTVmNTMzODJhMzJiMDEzMTFkMTZmODg1MGI1MTM3MjZjOGUifQ%3D%3D; _fbp=fb.2.1572241759237.1405095899; DETAIL[1][8334570]=1; user_index_role=1; user_browse_recent=a%3A1%3A%7Bi%3A0%3Ba%3A2%3A%7Bs%3A4%3A%22type%22%3Bi%3A1%3Bs%3A7%3A%22post_id%22%3Bs%3A7%3A%228334570%22%3B%7D%7D; ba_cid=a%3A5%3A%7Bs%3A6%3A%22ba_cid%22%3Bs%3A32%3A%2280d9a8e15314a3fb67dc8fbceb75e1d4%22%3Bs%3A7%3A%22page_ex%22%3Bs%3A0%3A%22%22%3Bs%3A4%3A%22page%22%3Bs%3A48%3A%22https%3A%2F%2Frent.591.com.tw%2Frent-detail-8334570.html%22%3Bs%3A7%3A%22time_ex%22%3Bs%3A0%3A%22%22%3Bs%3A4%3A%22time%22%3Bi%3A1572241969%3B%7D; __asc=0e60daee16e10ec8e498c3846ec; __auc=0e60daee16e10ec8e498c3846ec; localTime=1; _gat_UA-97423186-1=1',
             'Host': 'rent.591.com.tw',
-            'Referer': 'https://m.591.com.tw/v2/rent?regionid=3',
+            'Referer': 'https://rent.591.com.tw/v2/rent?regionid=3',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36' ,
             'X-CSRF-TOKEN': 'RNGEni8wMneYEXj7JeaB3GNnN8N8B8ufCaz9jiJ0',
             'X-Requested-With':'XMLHttpRequest'
@@ -86,7 +86,7 @@ try:
             gender = task2.find('性別要求')
             try:
                 if gender != -1:
-                    house_gender.append(task2[gender:gender+10].replace("性別要求：","").replace('朝向：','').replace('隔間材',''))
+                    house_gender.append(task2[gender:gender+10].replace("性別要求：","").replace('朝向：','').replace('隔間材','').replace('可遷入',''))
                 else:
                     house_gender.append("None")
             except:
